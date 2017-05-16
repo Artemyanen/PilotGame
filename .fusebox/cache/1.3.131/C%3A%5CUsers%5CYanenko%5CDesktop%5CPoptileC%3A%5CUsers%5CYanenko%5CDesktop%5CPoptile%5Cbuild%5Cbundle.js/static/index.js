bff845496705b2f7be5d486506afc2a207b1fb7a
Artem <artemyanen@gmail.com>
2017-05-16 11:46:45 +0200

    untrack .fusebox

--- .fusebox/cache/1.3.131/C%3A%5CUsers%5CYanenko%5CDesktop%5CPoptileC%3A%5CUsers%5CYanenko%5CDesktop%5CPoptile%5Cbuild%5Cbundle.js/static/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = { contents: "\"use strict\";\r\nexports.__esModule = true;\r\nvar Game_1 = require(\"./Core/Game\");\r\nvar game = new Game_1[\"default\"]();\r\ngame.start();\r\n//# sourceMappingURL=index.js.map",
-dependencies: ["./Core/Game"],
-sourceMap: "{\"version\":3,\"file\":\"index.js\",\"sourceRoot\":\"\",\"sources\":[\"index.ts\"],\"names\":[],\"mappings\":\";;AAAA,oCAA+B;AAE/B,IAAI,IAAI,GAAG,IAAI,iBAAI,EAAE,CAAC;AACtB,IAAI,CAAC,KAAK,EAAE,CAAC\",\"sourcesContent\":[\"import Game from './Core/Game';\\r\\n\\r\\nlet game = new Game();\\r\\ngame.start();\"]}",
-headerContent: undefined,
-mtime: 1493227655000
-};
